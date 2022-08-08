77f56dc7f249d0f160c56e2cc7af951d2dd18955
Winnie Mendoza <wrm956@gmail.com>
2022-08-08 16:29:53 -0400

    commit tree 0a274040f0a9c6dbcf21dd9715fa6f50f6b90ec1

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map { |name| badge_maker(name) }
+end
+
+def assign_rooms(speakers)
+    speakers.map.with_index(1) do |speaker, room|
+        "Hello, #{speaker}! You'll be assigned to room #{room}!"
+    end
+end
+
+def printer(speakers)
+    batch_badge_creator(speakers).map { |badge| puts badge }
+    assign_rooms(speakers).map { |assignment| puts assignment }
+end
